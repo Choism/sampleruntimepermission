60274cc198a31e81e3d9ca4b50e754664648ace6
SungJun Kim <smart23033@gmail.com>
2016-07-22 11:02:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tacademy/sampleruntimepermission/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.tacademy.sampleruntimepermission;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "save...", Toast.LENGTH_SHORT).show();
+                }
+        });
+
+        checkPermission(); // 액티비티 생성후 퍼미션 확인 호출
+    }
+
+    private void checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("permission");
+                builder.setMessage("permission .....");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        requestPermission();
+                    }
+                });
+                builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialogInterface) {
+                        finishNoPermission();
+                    }
+                });
+
+                builder.create().show();
+                return;
+            }
+            requestPermission();
+        }
+    }
+
+    private void finishNoPermission() {
+        Toast.makeText(this, "no permission", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    private static final int RC_PERMISSION = 100;
+    private void requestPermission() { // 퍼미션 흭득 요청
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, RC_PERMISSION);
+    }
+
+    @Override//흭득 결과 확인
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RC_PERMISSION) {
+            if (grantResults != null && grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "grant permission", Toast.LENGTH_SHORT).show();
+            } else {
+                finishNoPermission();
+            }
+        }
+    }
+}
